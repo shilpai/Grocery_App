119e0e8c1dbf197bef10099f632fae6fde2cff91
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2023-01-31 17:48:12 +0530

    minor change

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const routes = require('./Routes/Routes')
+const path = require('path')
 
+// static files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get("*", function ( req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// middleware
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -15,6 +24,7 @@ app.use(cors())
 
 app.use(routes)
 
+// listen port
 app.listen(port,()=>{
     console.log('listenig to port 3001')
 })
